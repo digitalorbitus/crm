230cd86da01add523feee36c4703df2f89bdb57f
M Aftab <aftabdigitalorbitus@gmail.com>
2026-09-16 20:42:11 +0500

    upadete-8

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -332,7 +332,7 @@ export default function Sidebar({
       icon: UserPlus,
       href: "/Attendance",
       hasDropdown: false,
-      roles: ["admin","user","staff", "agent" ],
+      roles: ["admin","user","staff", "agent","Desgin","SMM","Sales","Developer"],
     
     },
     {
